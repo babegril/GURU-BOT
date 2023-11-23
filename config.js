6bbb2197a6df554e6161d95478181b029fbdd7d6
Rathu chuti <87506366+babegril@users.noreply.github.com>
2023-11-23 14:47:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94770377817', 'SAVI', true],
+  ['94715773903', 'SAVI-MD', true], 
   [''] 
 ] //Number of owners
 
@@ -55,12 +55,12 @@ global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/n' 
+global.dygp = 'https://chat.whatsapp.com/'
+global.fgsc = 'https://github.com/babegril/GURU-BOT' 
+global.fgyt = 'https://youtube.com/@babegril'
+global.fgpyp = 'https://youtube.com/@babegril'
+global.fglog = 'https://raw.githubusercontent.com/babegril/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
